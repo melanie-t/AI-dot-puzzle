355491508b44bb57136dff2e58250e62ee20f2df
melanie-taing <>
2020-01-30 22:45:36 -0500

    Skeleton for depth first search

---
+++ depth_first_search.py
@@ -0,0 +1,11 @@
+def create_child_nodes(initial_node):
+    print("Creating child nodes")
+    # TODO Implement node creating
+
+
+size = ''
+max_d = ''
+max_l = ''
+initial = ''
+
+# TODO Implement txt file parser
