31a74a0209afd740b1436366f1b81504b2bf8863
melanie-taing <>
2020-02-05 11:48:03 -0500

    Modified function order

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,19 +1,4 @@
 import math
-
-def create_child_nodes(__initial_node, __open_list, __closed_list, __search_list):
-    # Creates children of the initial
-    print("Generated child nodes for ", __initial_node)
-    for token in range(0, len(__initial_node)):
-        __child_node = flip_adjacent_nodes(__initial_node, token)
-        # Check to see if the node exists already
-        __node_exists = __child_node in __open_list or __child_node in __closed_list
-        if not __node_exists:
-            # Add the child node to the open list and pop into search list stack
-            print("\t\tDiscovered", __child_node)
-            __open_list.append(__child_node)
-            __search_list.append(__child_node)
-
-
 def flip_token(token):
     if token == '0':
         return '1'
@@ -94,6 +79,21 @@ def flip_adjacent_nodes(board, index):
     return ''.join(new_board)
 
 
+def create_child_nodes(__initial_node, __open_list, __closed_list, __search_list):
+    # Creates children of the initial
+    print("Generated child nodes for ", __initial_node)
+    for token in range(0, len(__initial_node)):
+        __child_node = flip_adjacent_nodes(__initial_node, token)
+        # Check to see if the node exists already
+        __node_exists = __child_node in __open_list or __child_node in __closed_list
+        if not __node_exists:
+            # Add the child node to the open list and pop into search list stack
+            print("\t\tDiscovered", __child_node)
+            __open_list.append(__child_node)
+            __search_list.append(__child_node)
+
+
+
 def visit_next_node(__open_list, __closed_list, __search_list):
     __visited_node = __search_list.pop()
     __open_list.remove(__visited_node)
@@ -125,7 +125,6 @@ max_depth = 4
 current_depth = 1
 open_list.append(initial_board)
 search_list.append(initial_board)
-
 solved = False
 
 # TODO Position conversion given an index
