4259816b1e66b4243cbc50386c12ef8f258a32ea
melanie-taing <>
2020-02-07 15:41:45 -0500

    For loop for iterating through all the puzzles in main method

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import depth_first_search
 import read_puzzle_file
 
+# TODO @Patrick, remove these comments below when you're done with input file
+# n = 3
+# max_d = 100
+# puzzle = "111001011"
+# puzzle_name = "puzzle_1"
+
+puzzles = [[3, 100, 111001011]]
 
 # Initialize variables
-n = 3
-max_d = 100
-puzzle = "111001011"
-puzzle_name = "puzzle_1"
+for i in range(0, len(puzzles)):
+    n = int(puzzles[i][0])
+    max_d = int(puzzles[i][1])
+    puzzle = str(puzzles[i][2])
+    puzzle_name = "puzzle_" + str(i+1)
 
-depth_first_search.search(n, max_d, puzzle, puzzle_name)
-
+    depth_first_search.search(n, max_d, puzzle, puzzle_name)
 
