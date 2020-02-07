6aee5cbf28cbe52f5e4007807b283ea727c5adc4
melanie-taing <>
2020-02-06 22:16:06 -0500

    Moved main method to a separate file

--- depth_first_search.py
+++ depth_first_search.py
@@ -1,5 +1,3 @@
-import math
-
 # TODO File output
 # TODO Solution path
 # TODO Read file and input into variables
@@ -20,13 +18,12 @@ def flip_token(token):
         return '0'
 
 
-def flip_adjacent_nodes(board, index):
+def flip_adjacent_nodes(board, index, size_n):
     # Copy the board to modify the new board
     new_board = list(board)
-    n = int(math.sqrt(len(new_board)))
-    max_index = n-1
-    token_above = index-n
-    token_below = index+n
+    max_index = size_n - 1
+    token_above = index - size_n
+    token_below = index + size_n
     token_left = index-1
     token_right = index+1
 
@@ -43,26 +40,26 @@ def flip_adjacent_nodes(board, index):
         new_board[token_below] = flip_token(new_board[token_below])
 
     # Bottom left corner
-    elif index == n*max_index:
+    elif index == size_n*max_index:
         new_board[index] = flip_token(new_board[index])
         new_board[token_above] = flip_token(new_board[token_above])
         new_board[token_right] = flip_token(new_board[token_right])
 
     # Bottom right Corner
-    elif index == n*n-1:
+    elif index == size_n*size_n-1:
         new_board[index] = flip_token(new_board[index])
         new_board[token_above] = flip_token(new_board[token_above])
         new_board[token_left] = flip_token(new_board[token_left])
 
     # Left edge
-    elif index % n == 0:
+    elif index % size_n == 0:
         new_board[index] = flip_token(new_board[index])
         new_board[token_above] = flip_token(new_board[token_above])
         new_board[token_below] = flip_token(new_board[token_below])
         new_board[token_right] = flip_token(new_board[token_right])
 
     # Right edge
-    elif (index+1) % n == 0:
+    elif (index+1) % size_n == 0:
         new_board[index] = flip_token(new_board[index])
         new_board[token_above] = flip_token(new_board[token_above])
         new_board[token_below] = flip_token(new_board[token_below])
@@ -76,7 +73,7 @@ def flip_adjacent_nodes(board, index):
         new_board[token_below] = flip_token(new_board[token_below])
 
     # Bottom edge
-    elif int(index/n) == max_index:
+    elif int(index / size_n) == max_index:
         new_board[index] = flip_token(new_board[index])
         new_board[token_above] = flip_token(new_board[token_above])
         new_board[token_left] = flip_token(new_board[token_left])
@@ -93,18 +90,18 @@ def flip_adjacent_nodes(board, index):
     return ''.join(new_board)
 
 
-def create_child_nodes(initial_node, open_list, closed_list, moves_dict, current_depth):
+def create_child_nodes(initial_node, open_list, closed_list, moves_dict, current_depth, size_n):
     # Creates children of the initial
     print("Generated child nodes for ", initial_node)
     sorted_children = []
     for token in range(0, len(initial_node)):
-        child_node = flip_adjacent_nodes(initial_node, token)
+        child_node = flip_adjacent_nodes(initial_node, token, size_n)
         # Check to see if the node exists already
         node_exists = child_node in open_list or child_node in closed_list
         if not node_exists:
             # Add the child node to the open list and pop into search list stack
             print("\t\tDiscovered", child_node)
-            moves_dict[child_node] = [current_depth, position(token, math.sqrt(len(initial_node)))]
+            moves_dict[child_node] = [current_depth, position(token, size_n)]
             sorted_children.append(child_node)
         elif child_node in open_list:
             node_depth = (moves_dict[child_node][0])
@@ -118,48 +115,44 @@ def create_child_nodes(initial_node, open_list, closed_list, moves_dict, current
     open_list.extend(sorted_children)
 
 
-def visit_next_node(open_list, closed_list, moves_dict, search_path, current_depth, max_depth):
+def visit_next_node(open_list, closed_list, moves_dict, search_path, solution_path, current_depth, max_depth):
     visited_node = open_list.pop()
-    updated_depth = moves_dict.pop(visited_node)
+    node_info = moves_dict.pop(visited_node)
     search_path.append(visited_node)
     # If the child node is not at max depth, then add to closed_list (meaning the node was expanded already)
     if not current_depth == max_depth:
         closed_list.append(visited_node)
-    print("Visit node", visited_node, "| Depth: ", updated_depth)
+    print("Visit node", visited_node, "| Depth: ", node_info)
 
     # Goal state
     if visited_node.find("1") == -1:
         closed_list.append(visited_node)
         open_list.clear()   # clear for the stopping condition when open_list length is 0
+        solution_path[visited_node] = node_info[1]
         return [visited_node, [-1, '0']]
 
-    return [visited_node, updated_depth]
+    return [visited_node, node_info]
 
 
-def main():
+def search(size_n, max_d, initial_board):
     moves_dict = dict()
     closed_list = []
     open_list = []
     search_path = []
-    solution_path = []
-
-    # Initial board set up
-    n = 2
-    initial_board = "0110"
-
-    n = 3
-    max_d = 20
-    initial_board = "111001011"
+    solution_path = dict()
+    child_list = []
 
     current_depth = 1
     open_list.append(initial_board)
     search_path.append(initial_board)
     moves_dict[initial_board] = [1, 0]
+    solution_path[initial_board] = 0
     solved = False
 
     while not len(open_list) == 0 and not solved:
         # Visit node
-        visited_node = visit_next_node(open_list, closed_list, moves_dict, search_path, current_depth, max_d)
+        visited_node = visit_next_node(open_list, closed_list, moves_dict, search_path,
+                                       solution_path, current_depth, max_d)
         current_depth = (visited_node[1])[0]
         # Check if the goal state is returned
         if current_depth == -1:
@@ -170,7 +163,7 @@ def main():
             # Update the current_depth since we have generated child_nodes
             current_depth += 1
             # Generate children nodes, sort the children nodes and add to the open list
-            create_child_nodes(visited_node[0], open_list, closed_list, moves_dict, current_depth)
+            create_child_nodes(visited_node[0], open_list, closed_list, moves_dict, current_depth, size_n)
             print("Updated depth:", current_depth)
             print("Closed list (" + str(len(closed_list)) + ")", closed_list)
             print("Open list (" + str(len(open_list)) + ")", open_list)
@@ -187,8 +180,3 @@ def main():
             print("Solution path(" + str((len(solution_path))) + ")", solution_path)
         else:
             print("No solution")
-
-
-if __name__ == '__main__':
-    main()
-
