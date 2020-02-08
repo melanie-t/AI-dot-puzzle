3257286b3bb69521fa813b9c839cff3ee3ee9b95
melanie-t <>
2020-02-08 16:23:31 -0500

    Factor out file read code into its own function and created main function

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import depth_first_search
 
-print("================================================")
-print("|| Welcome to A* Is Born's Depth First Search ||")
-print("================================================")
 
-puzzles = [] # Puzzles specified outside to keep all games separate
-while True:
-    print("\nInput quit to terminate the application")
-    input_file_path = str(input("Enter the path of game info text file (ex: input/games.txt): "))
-    if input_file_path == "quit":
-        break
+def file_read(path, puzzles):
     try:
-        game_file = open(input_file_path)
+        game_file = open(path)
         # use readline() to read the first line
         for line in game_file:
             game_info = line.replace("\n", "").split(" ")
@@ -25,14 +17,30 @@ while True:
     finally:
         game_file.close()
 
-    # Initialize variables
-    for i in range(0, len(puzzles)):
-        n = int(puzzles[i][0])
-        max_d = int(puzzles[i][1])
-        puzzle = str(puzzles[i][3])
-        puzzle_name = "puzzle_" + str(i+1)
-        print_steps_enabled = False     # False for faster run time
 
-        depth_first_search.search(n, max_d, puzzle, puzzle_name, print_steps_enabled)
+def main():
+    print("================================================")
+    print("|| Welcome to A* Is Born's Depth First Search ||")
+    print("================================================")
 
-    # puzzles.clear()
+    puzzles = []  # Puzzles specified outside to keep all games separate
+    while True:
+        print("\nInput quit to terminate the application")
+        user_input = str(input("Enter the path of game info text file (ex: input/games.txt): "))
+        if user_input == "quit":
+            break
+        else:
+            file_read(user_input, puzzles)
+        # Initialize variables
+        for i in range(0, len(puzzles)):
+            n = int(puzzles[i][0])
+            max_d = int(puzzles[i][1])
+            puzzle = str(puzzles[i][3])
+            puzzle_name = "puzzle_" + str(i + 1)
+            print_steps_enabled = False  # False for faster run time
+
+            depth_first_search.search(n, max_d, puzzle, puzzle_name, print_steps_enabled)
+
+
+if __name__ == '__main__':
+    main()
