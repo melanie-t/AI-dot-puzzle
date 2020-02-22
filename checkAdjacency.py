3eeaa65500c24d1f85d9d406d15ea99427796962
Pakman888 <patrick.ph.chan@gmail.com>
2020-02-22 16:23:45 -0500

    corrected adjacent detection
    should work perfectly now

--- checkAdjacency.py
+++ checkAdjacency.py
@@ -6,52 +6,63 @@ def get_neighbour_number(str_puzzle, char_index):
     num_black = 0
 
     n_size = math.sqrt(len(str_puzzle))
-    print("string length: " + str(len(str_puzzle)))
-    print("n-size: "+str(int(n_size)))
-    print("piece on index: "+str_puzzle[char_index])
-    print("piece index: " + str(char_index))
+#    print("string length: " + str(len(str_puzzle)))
+#    print("n-size: "+str(int(n_size)))
+#    print("piece on index: "+str_puzzle[char_index])
+    if (str_puzzle[char_index] == "0"):
+        num_white += 1
+    else:
+        num_black += 1
+#    print("piece index: " + str(char_index))
 
-    if(((char_index% n_size) -1)>=0):
-        print("left from index is index: "+ str(char_index -1))
-        print("piece: " + str_puzzle[char_index -1])
+    if(((char_index% n_size))>0):
+#        print("left from index is index: "+ str(char_index -1))
+#        print("piece: " + str_puzzle[char_index -1])
         if(str_puzzle[char_index -1] == "0"):
             num_white += 1
         else:
             num_black += 1
 
-
-    if (((char_index % n_size)+1) >0):
-        print("right from index is index: " + str(char_index + 1))
-        print("piece: " + str_puzzle[char_index + 1])
+    if (((char_index+1) % n_size) >0):
+#        print("right from index is index: " + str(char_index + 1))
+#        print("piece: " + str_puzzle[char_index + 1])
         if (str_puzzle[char_index + 1] == "0"):
             num_white += 1
         else:
             num_black += 1
 
-    if (((char_index%n_size)+n_size) >=0):
-        print("top from index is index: " + str(int((char_index%n_size)+n_size)))
-        print("piece: " + str_puzzle[int((char_index%n_size)+n_size)])
-        if (str_puzzle[int((char_index%n_size)+n_size)] == "0"):
+
+    if (((char_index-n_size)) >=0):
+#        print("top from index is index: " + str(int((char_index-n_size))))
+#        print("piece: " + str_puzzle[int(char_index-n_size)])
+        if (str_puzzle[int(char_index-n_size)] == "0"):
             num_white += 1
         else:
             num_black += 1
 
-    if (((char_index+ (n_size)) % n_size) >=0):
-        print("bot from index is index: " + str((char_index+ (n_size))))
-        print("piece: " + str_puzzle[int(char_index+ (n_size))])
+    if (((char_index+ (n_size))< len(str_puzzle))):
+#        print("bot from index is index: " + str((char_index+ (n_size))))
+#        print("piece: " + str_puzzle[int(char_index+ (n_size))])
         if (str_puzzle[int(char_index+ (n_size))] == "0"):
             num_white += 1
         else:
             num_black += 1
-    #print("#black in def: "+ str(num_black))
-    return num_black, num_white
 
-blk, wht = get_neighbour_number("0110111100010101", 10)
+    ratio = num_white-num_black  # 1 more white, return -1
+#    print("black pieces: "+ str(num_black))
+#    print("white pieces: "+ str(num_white))
+
+#    print("blk-white ratio: "+str(ratio))
+  #  h_pt = ratio*h_weight
+    return ratio ##don't wnat a - heuristic
+
+#heuristic_pt = get_neighbour_number("0110111100010101", 4) #heuristic is 13.33
+heuristic_pt = get_neighbour_number("0110111100010101", 7) #heuristic is 0
+#heuristic_pt = get_neighbour_number("111101111", 4) #heuristic is 0
 
 #0110->3
 #1111->7
 #0001->11
 #0101->15
 
-print("black pieces: "+ str(blk))
-print("white pieces: "+ str(wht))
+print("heuristic: "+ str(heuristic_pt))
