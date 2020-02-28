353b06a68f45d47732af3a064400df83e049173a
melanie-t <melanie.taing@mail.concordia.ca>
2020-02-28 00:00:04 -0500

    Heuristic to calculate the max adjacent black pieces as a ratio

--- src/heuristics.py
+++ src/heuristics.py
@@ -1,8 +1,10 @@
+from math import sqrt
 
 
 def calculate_heuristics(board):
-    return number_of_black(board)
+    # return number_of_black(board)
     # return number_of_black_over_five(board)
+    return max_adjacent_black(board)
 
 
 def number_of_black(board):
@@ -13,3 +15,45 @@ def number_of_black(board):
 def number_of_black_over_five(board):
     num_black = board.count("1")
     return int(num_black/5)
+
+
+def max_adjacent_black(board):
+    length = int(len(board))
+    n = int(sqrt(length))
+    number_of_blacks = number_of_black(board)
+    max_adjacent = 0
+
+    # Solution found
+    if number_of_blacks == 0:
+        return 0
+
+    # Iterate through the whole board
+    for i in range(0, length):
+        adjacent_black = 0
+        token_above = i - n
+        token_below = i + n
+        token_left = i - 1
+        token_right = i + 1
+
+        # Only check for adjacent if the token itself is black
+        if board[i] == '1':
+            adjacent_black += 1
+            # Check for adjacent
+            if token_above > 0:
+                if board[token_above] == '1':
+                    adjacent_black += 1
+            if token_below < length:
+                if board[token_below] == '1':
+                    adjacent_black += 1
+            if token_left > 0:
+                if board[token_left] == '1':
+                    adjacent_black += 1
+            if token_right < length:
+                if board[token_right] == '1':
+                    adjacent_black += 1
+
+        # If it found an adjacent, then +1 to account for the token itself
+        if adjacent_black > max_adjacent:
+            max_adjacent = adjacent_black
+
+    return (int(number_of_blacks/max_adjacent))*number_of_blacks
