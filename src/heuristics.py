6ccb9ea2d351ca5a35852141037b8bc8ce20198a
melanie-t <melanie.taing@mail.concordia.ca>
2020-03-01 03:13:52 -0500

    Update heuristic #2 to multiply the number of black pieces

--- src/heuristics.py
+++ src/heuristics.py
@@ -14,7 +14,7 @@ def number_of_black(board):
 
 def number_of_black_over_five(board):
     num_black = board.count("1")
-    return int(num_black/5)
+    return int((num_black/5)*num_black)
 
 
 def max_adjacent_black(board):
